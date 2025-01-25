17bbaea9f340f15a54369291ed0ce66e2660baea
Chad Weimer <1165476+chadweimer@users.noreply.github.com>
2025-01-25 12:02:24 -0800

    Wrong args...

--- postgres_upgrade.sh
+++ postgres_upgrade.sh
@@ -52,7 +52,7 @@ chown postgres:postgres "/data/pg${NEWVER}"
 cd "/data/pg${NEWVER}"
 
 # init new db
-gosu postgres initdb -u ${PGUSER} -D "/data/pg${NEWVER}" --locale=en_US.UTF-8 --encoding=UTF8
+gosu postgres initdb -U ${PGUSER} -D "/data/pg${NEWVER}" --locale=en_US.UTF-8 --encoding=UTF8
 
 # run the upgrade
-gosu postgres pg_upgrade -u ${PGUSER} -b "/usr/libexec/postgresql${OLDVER}/" -B "/usr/libexec/postgresql${NEWVER}/" -d "/data/pg${OLDVER}/" -D "/data/pg${NEWVER}/"
+gosu postgres pg_upgrade -b "/usr/libexec/postgresql${OLDVER}/" -B "/usr/libexec/postgresql${NEWVER}/" -d "/data/pg${OLDVER}/" -D "/data/pg${NEWVER}/"
