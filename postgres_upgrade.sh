a22606092bc67313956cbdc0a36f7e11b2ac359f
Chad Weimer <1165476+chadweimer@users.noreply.github.com>
2025-01-25 12:13:39 -0800

    Use a password

--- postgres_upgrade.sh
+++ postgres_upgrade.sh
@@ -8,6 +8,7 @@ NEWVER="${NEWVER:-}"
 
 # superuser for database
 PGUSER="${PGUSER:-postgres}"
+PGPASSWORD="${PGPASSWORD:-}"
 
 # make sure old directory exists
 if [ ! -d "/data/pg${OLDVER}" ]
@@ -52,7 +53,8 @@ chown postgres:postgres "/data/pg${NEWVER}"
 cd "/data/pg${NEWVER}"
 
 # init new db
-gosu postgres initdb -U ${PGUSER} -D "/data/pg${NEWVER}" --locale=en_US.UTF-8 --encoding=UTF8
+cat $PGPASSWORD > .pgpassword
+gosu postgres initdb -U ${PGUSER} --pwfile=.pgpassword -D "/data/pg${NEWVER}" --locale=en_US.UTF-8 --encoding=UTF8
 
 # run the upgrade
 gosu postgres pg_upgrade -b "/usr/libexec/postgresql${OLDVER}/" -B "/usr/libexec/postgresql${NEWVER}/" -d "/data/pg${OLDVER}/" -D "/data/pg${NEWVER}/"
